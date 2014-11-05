688eceaeaf6560f687a9dc3bf3c8c948e9f9f408
THERESA BREINER <THERESA.BREINER@GMAIL.COM>
2014-11-05 18:51:26 -0500

    After class on Wednesday. We've fixed the space issue and deleted unnecessary functions. working on filling out tests.

--- NegadecimalCalculatorTest.java
+++ NegadecimalCalculatorTest.java
@@ -9,48 +9,72 @@ public class NegadecimalCalculatorTest {
 
 	NegadecimalNumber ndn1;
 	NegadecimalNumber ndn2;
+	NegadecimalNumber ndn3; 	//keep it negative
+	NegadecimalNumber ndn4; 	//keep it negative
 	NegadecimalNumber answer;
+	NegadecimalCalculator calc;
 	
 	@Before
 	public void setUp() {
+		calc = new NegadecimalCalculator();
 		ndn1 = new NegadecimalNumber(256);
 		ndn2 = new NegadecimalNumber(300);
 		answer = new NegadecimalNumber(0);
+		
+		ndn3 = new NegadecimalNumber(-120);
+		ndn4 = new NegadecimalNumber(-80);
 	}
-	
-	@Test
-	public void testMain() {
-		fail("Not yet implemented");
-	}
-
 	
 	
 	@Test
 	public void testDoArithmetic() {
+		calc.currentNDN = ndn1;
 		answer = new NegadecimalNumber(556);
-		//NegadecimalCalculator.setCurrentNDN(ndn1);
-		//assertEquals(answer, NegadecimalCalculator.doArithmetic('+', ndn2));
+		assertTrue(answer.equals(calc.doArithmetic('+', ndn2)));
 		
-		/*setCurrentNDN(ndn2);
-		 * answer = new NegadecimalNumber(44);
-		assertEquals(answer, doArithmetic('-', ndn1));
+		calc.currentNDN = ndn3;
+		answer = new NegadecimalNumber(-200);
+		assertTrue(answer.equals(calc.doArithmetic('+', ndn4)));
 		
-		answer = new NegadecimalNumber(76800);
-		assertEquals(answer, doArithmetic('*', ndn1);
+		calc.currentNDN = ndn2;
+		answer = new NegadecimalNumber(220);
+		assertTrue(answer.equals(calc.doArithmetic('+', ndn4)));
 		
-		setCurrentNDN(answer);
-		assertEquals(ndn1, doArithmetic(ndn2));
-		
-		setCurrentNDN(ndn2);
+		calc.currentNDN = ndn2;
 		answer = new NegadecimalNumber(44);
-		assertEquals(answer, doArithmetic(ndn1);
-		*/
+		assertTrue(answer.equals(calc.doArithmetic('-', ndn1)));
+		
+		answer = new NegadecimalNumber(380);
+		assertTrue(answer.equals(calc.doArithmetic('-', ndn4)));
+		
+		calc.currentNDN = ndn3;
+		answer = new NegadecimalNumber(-40);
+		assertTrue(answer.equals(calc.doArithmetic('-', ndn4)));
+		
+		answer = new NegadecimalNumber(-30720);
+		assertTrue(answer.equals(calc.doArithmetic('*', ndn1)));
+		
+		calc.currentNDN = answer;
+		assertTrue(ndn1.equals(calc.doArithmetic('/', ndn3)));
+		
+		calc.currentNDN = ndn1;
+		answer = new NegadecimalNumber(76800);
+		assertTrue(answer.equals(calc.doArithmetic('*',  ndn2)));
+		
+		calc.currentNDN = answer;
+		assertTrue(ndn2.equals(calc.doArithmetic('/', ndn1)));
+		
+		calc.currentNDN = ndn2;
+		answer = new NegadecimalNumber(44);
+		assertTrue(answer.equals(calc.doArithmetic('%', ndn1)));
+		
 		
 	}
 
 	@Test
 	public void testFindSecondOperand() {
-		fail("Not yet implemented");
+		assertEquals("982", calc.findSecondOperand(" 982"));
+		assertEquals("714", calc.findSecondOperand("714"));
 	}
 
 	@Test
@@ -58,24 +82,13 @@ public class NegadecimalCalculatorTest {
 		fail("Not yet implemented");
 	}
 
-	@Test
-	public void testFindOperatorIndex() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testBadInputForSimple() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetInput() {
-		fail("Not yet implemented");
-	}
-
+	
 	@Test
 	public void testDoDecimalInteger() {
-		fail("Not yet implemented");
+		assertEquals("125", calc.doDecimalInteger(" 85"));
+		assertEquals("125", calc.doDecimalInteger("85"));
+		assertEquals("125", calc.doDecimalInteger("ecimal85"));
+		assertEquals("125", calc.doDecimalInteger("ecimal 85"));
 	}
 
 	@Test
@@ -83,4 +96,9 @@ public class NegadecimalCalculatorTest {
 		fail("Not yet implemented");
 	}
 
+	
+	public void setCurrentNDN(NegadecimalNumber ndn) {
+		calc.currentNDN = ndn;
+		return;
+	}
 }
